779e7f84f63d16676191f1c14b7af2754c0f13cd
Janeal Pimentel <doodlerprizz@hotmail.ca>
2020-09-06 18:09:31 -0500

    Adds hello command

---
+++ app/commands/hello.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require("discord.js");
+require("../utils/general/helper");
+
+module.exports = {
+  name: "hello",
+  cooldown: 3,
+  aliases: ["hi", "yo", "hola", "konnichiwa", "hey", "wassup", "sup", "aloha"],
+  description: "Say hello!",
+  execute(msg) {
+    const { author, guild } = msg;
+    const message = new MessageEmbed()
+      .setAuthor(
+        `${module.exports.aliases[
+          Math.floor(Math.random() * module.exports.aliases.length)
+        ].toProperCase()} ${author.username}!`
+      )
+      .setTimestamp()
+      .setFooter(
+        `${guild.name}`,
+        `${guild.iconURL({ format: "png", dynamic: true })}`
+      );
+
+    msg.channel.send(message);
+  },
+};
