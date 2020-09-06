5ab383c886e6d6a643077423c8574279bb9ce41b
Janeal Pimentel <doodlerprizz@hotmail.ca>
2020-09-06 18:09:15 -0500

    Adds toProperCase String Function

---
+++ app/utils/general/helper.js
@@ -0,0 +1,6 @@
+// To TitleCase
+String.prototype.toProperCase = function () {
+  return this.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+};
