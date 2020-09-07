b2848c8ea69b734c217683701ee3619908df0c66
Janeal Pimentel <ceejaypimentel@hotmail.ca>
2020-09-07 16:13:12 -0500

    Adds new changes to cat

--- app/commands/cat.js
+++ app/commands/cat.js
@@ -9,23 +9,26 @@ module.exports = {
   description: "Cat pictures!",
   execute(msg) {
     const { author, guild } = msg;
-    msg.channel.send("Generating Picture of Cat...").then((b_msg) => {
-      const url = "https://aws.random.cat/meow";
-      const title = `${guild.name} Cats!`;
-      get(url).then((res) => {
-        const picture = res.data;
-        const message = new MessageEmbed()
-          .setColor(colors.lightGreen.light)
-          .setAuthor(title, guild.iconURL)
-          .setImage(picture.file)
-          .setTimestamp()
-          .setFooter(
-            `${msg.client.user.tag} • Created by OnPaper`,
-            msg.client.user.displayAvatarURL()
-          );
+    msg.channel
+      .send("Generating Picture of Cat...")
+      .then((b_msg) => {
+        const url = "https://aws.random.cat/meow";
+        const title = `${guild.name} Cats!`;
+        get(url).then((res) => {
+          const picture = res.data;
+          const message = new MessageEmbed()
+            .setColor(colors.lightGreen.light)
+            .setAuthor(title, guild.iconURL())
+            .setImage(picture.file)
+            .setTimestamp()
+            .setFooter(
+              `${msg.client.user.tag} • Created by OnPaper`,
+              msg.client.user.displayAvatarURL()
+            );
 
-        b_msg.edit({ embed: message });
-      });
-    });
+          b_msg.edit(message).catch(console.error);
+        });
+      })
+      .catch(console.error);
   },
 };
