30458e3f2d095d93fa58d4ed191d4cda919e658c
Janeal Pimentel <ceejaypimentel@hotmail.ca>
2020-09-07 16:13:22 -0500

    Adds new command dog

---
+++ app/commands/dog.js
@@ -0,0 +1,33 @@
+const { MessageEmbed } = require("discord.js");
+const { get } = require("axios");
+const colors = require("../utils/general/colors");
+
+module.exports = {
+  name: "dog",
+  cooldown: 5,
+  aliases: ["doggo", "d", "pup", "puppy", "aso"],
+  execute(msg) {
+    const { guild, client } = msg;
+    msg.channel
+      .send("Generating Picture of Dog...")
+      .then((b_msg) => {
+        const url = "https://dog.ceo/api/breeds/image/random";
+        const title = `${guild.name} Dogs!`;
+        get(url).then((res) => {
+          const picture = res.data;
+          const message = new MessageEmbed()
+            .setColor(colors.lightBlue.light)
+            .setAuthor(title, guild.iconURL())
+            .setImage(picture.message)
+            .setTimestamp()
+            .setFooter(
+              `${msg.client.user.tag} • Created by OnPaper`,
+              msg.client.user.displayAvatarURL()
+            );
+
+          b_msg.edit(message).catch(console.error);
+        });
+      })
+      .catch(console.error);
+  },
+};
